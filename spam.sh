77eb71f68548c4d5f86db6a3da55c10c4492a6b2
Benji Wallis <benjiwallis2008@gmail.com>
2023-06-12 12:07:59 +1200

    added amount of message option

--- spam.sh
+++ spam.sh
@@ -6,8 +6,11 @@ read -p "Enter phone number to spam: " phone_number
 # Prompt user for message to send
 read -p "Enter message to send: " message
 
+#Prompt user for number of messages to send
+read -p "Enter number of messages to send: " number_of_messages
+
 # Loop to send message multiple times
-for i in {1..10}
+for i in $(seq 1 $number_of_messages)
 do
   osascript -e "tell application \"Messages\" to send \"$message\" to buddy \"$phone_number\""
 done
