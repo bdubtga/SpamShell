24641046a74a75f69c61eb72010d5ad895823184
Benji Wallis <benjiwallis2008@gmail.com>
2023-06-12 12:12:31 +1200

    added install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copy spam.sh to /usr/local/bin
+sudo cp spam.sh /usr/local/bin/spam
+
+# Make spam.sh executable
+sudo chmod +x /usr/local/bin/spam
+
+# Add /usr/local/bin to PATH in .zshrc
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
+
+# Source .zshrc to update PATH
+source ~/.zshrc
+
+echo "spam command installed successfully!"
